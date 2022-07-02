b1dcbb66646fbb34d01becae04bff08cf6ee04cb
m3rashid <mdrashid.hussain.786.01@gmail.com>
2022-07-02 16:10:30 +0530

    add

--- june_15/numpy_conv.py
+++ june_15/numpy_conv.py
@@ -1,1 +1,10 @@
 import numpy as np
+import torch
+
+# convert numpy array back to torch tensor
+numpy_arr = np.zeros((5, 5))
+tensor = torch.from_numpy(numpy_arr)
+print(tensor)
+
+# convert back to numpy array
+arr = tensor.numpy()
